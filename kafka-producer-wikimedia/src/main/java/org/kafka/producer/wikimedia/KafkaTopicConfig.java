3d07d9aab8acb1e775c9bca3a966d3924eefc998
ricardomoras <ricardo.mora.sanchez@gmail.com>
2024-10-10 16:07:48 -0600

    start project

---
+++ kafka-producer-wikimedia/src/main/java/org/kafka/producer/wikimedia/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package org.kafka.producer.wikimedia;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+	@Bean
+	public NewTopic Topic() {
+		return TopicBuilder.name("wikimedia_recentchange")
+				.build();
+	}
+}
